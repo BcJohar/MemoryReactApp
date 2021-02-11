4f687c1f2b2e8838603037482d4d0befbd23a959
Bouchaib <joharchouaib@gmail.com>
2021-02-11 21:18:43 +0100

    first commit

---
+++ src/GuessCount.js
@@ -0,0 +1,7 @@
+import './GuessCount.css'
+import PropTypes from 'prop-types'
+const GuessCount = ({guesses}) => <div className="guesses">{guesses}</div>
+GuessCount.propTypes = {
+    guesses: PropTypes.number.isRequired,
+  }
+export default GuessCount;
